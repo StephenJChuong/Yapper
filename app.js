492b74fb33a356667542b914fa0b0f6badc17e14
StephenJChuong <johnchuong@protonmail.com>
2021-04-28 17:11:05 +0300

    New

--- app.js
+++ app.js
@@ -6,8 +6,7 @@ var server = http,createServer(function(req, res){
     res.writeHead(200, {'Content-type': 'text/html'});
     var myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
     myReadStream.pipe(res);
-
-});
+ });
 
 server.listen(3000, '127.0.0.1');
 console.log('yo dawgs, now listening to port 3000')
